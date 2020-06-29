0a9dc9c11501688a87db792d18ca89550b7576f6
brunoming <brunoming1996@qq.com>
2020-06-29 15:37:47 +0800

    Updates

---
+++ precache-manifest.3359fc835e84682169367edc5b222c95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c48293676dacb344504be0e5c1df32",
+    "url": "/brunoink/index.html"
+  },
+  {
+    "revision": "8c3659ccccbc147f6396",
+    "url": "/brunoink/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "6b0ce8f1f919ad9c8285",
+    "url": "/brunoink/static/css/main.44a4eafb.chunk.css"
+  },
+  {
+    "revision": "8c3659ccccbc147f6396",
+    "url": "/brunoink/static/js/2.73159451.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brunoink/static/js/2.73159451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0ce8f1f919ad9c8285",
+    "url": "/brunoink/static/js/main.3ed84f46.chunk.js"
+  },
+  {
+    "revision": "5b94cdd64704812b34ad",
+    "url": "/brunoink/static/js/runtime-main.c8a6437c.js"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/brunoink/static/media/Lobster-Regular.9406d0ab.ttf"
+  }
+]);
